152f697fa58a46e0c87dd1058143a8059c622383
jhSantiagoDev <jhc.santiago420@gmail.com>
2024-06-25 15:16:45 -0600

    v1

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { CustomerReviews } from "./components/CustomerReviews";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
+import { PopularProducts } from "./components/PopularProducts";
+import { Services } from "./components/Services";
+import { SpecialOffer } from "./components/SpecialOffer";
+import { Subscribe } from "./components/Subscribe";
+import { SuperQuality } from "./components/SuperQuality";
+
+function App() {
+  return (
+    <main className="relative">
+      <Navbar />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t bg-black pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+}
+
+export default App;
