75b4d498d8306be211761cbb671afea2ee560441
jhSantiagoDev <jhc.santiago420@gmail.com>
2024-07-02 12:04:40 -0600

    quality finish

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,14 @@ import React from "react";
 export const Button = ({ label, iconUrl }) => {
   return (
     <button
-      className="flex justify-center items-centergap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+      className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
                 bg-coral-red rounded-full text-white border-coral-red"
     >
       {label}
 
-      <img src={iconUrl} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+      {iconUrl && (
+        <img src={iconUrl} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
   );
 };
--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { Button } from "./Button";
+import { shoe8 } from "../assets/images";
 
 export const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section className="w-full flex justify-between items-center max-lg:flex-col gap-10 max-container">
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl font-bold capitalize lg:max-w-lg">
+          We provide you
+          <span className=" text-coral-red inline-block mx-2">super </span>
+          <span className=" text-coral-red inline-block mr-2">quality</span>
+          Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort. and innovation four
+          you active life.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe"
+          className="object-contain"
+          width={570}
+          height={522}
+        />
+      </div>
+    </section>
+  );
 };
